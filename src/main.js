8139e7e5c469f06c0b59dd0ae786897e6234c3bd
Daniel Newby <daniel.newby@apdcomms.co.uk>
2020-05-12 10:17:29 +0100

    added git activity

--- src/main.js
+++ src/main.js
@@ -4,20 +4,22 @@ import './../node_modules/bulma/css/bulma.css';
 import './assets/global.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faEnvelopeSquare, faPalette } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelopeSquare, faPalette, faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookSquare, faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 import router from './router';
 import 'es6-promise/auto';
 import Vuex from 'vuex';
 import store from "./store";
+import axios from 'axios'
 
-library.add(faFacebookSquare, faLinkedin, faGithubSquare, faEnvelopeSquare, faPalette);
+library.add(faFacebookSquare, faLinkedin, faGithubSquare, faEnvelopeSquare, faPalette, faCodeBranch);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(Vuex);
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   store,
