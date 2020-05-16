54684593eae9b7b7da0a40e79bf4d0ddccd7b91f
Daniel Newby <daniel.newby@apdcomms.co.uk>
2020-05-16 17:32:19 +0100

    test for the new api

--- src/components/BrainDump.vue
+++ src/components/BrainDump.vue
@@ -2,20 +2,31 @@
   <div class="tile is-ancestor">
     <div class="tile is-parent is-vertical">
       <div class="tile is-child box has-text-centered">
-        <p class="title">Coming soon-ish</p>
+        <div v-for="note in notes.slice(0, 5)" :key="note.title">
+          <p class="title">{{ note.title }}</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "BrainDump"
+  name: "BrainDump",
+  data() {
+    return {
+      notes: []
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.n3wby.co.uk/api/notes")
+      .then(response => (this.notes = response.data));
+  }
 };
 </script>
 
 <style scoped>
-.footer {
-  padding: 3rem 1.5rem;
-}
 </style>
